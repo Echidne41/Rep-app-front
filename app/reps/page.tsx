37fee88326f6cc0c08ef54de6118f398638abe0c
Echidne41 <nicolembutler@gmail.com>
2025-09-20 10:46:55 -0400

    Update page.tsx

--- app/reps/page.tsx
+++ app/reps/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useEffect, useMemo, useState } from 'react'
+import AddressSuggest from '@/components/AddressSuggest';
 
 /* ========= Types ========= */
 type Rep = {
